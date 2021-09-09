ce3d579a0eddea460945051d396248038370404a
Reza <99068159@mydavinci.nl>
2021-09-09 22:03:13 +0200

    twee foto's ge add

--- Feestlucnch.py
+++ Feestlucnch.py
@@ -2,4 +2,4 @@ crossaintjes = 0.39
 stokbroden = 2.78
 kortingsbonnen = 0.50
 
-print( crossaintjes * 17 + stokbroden * 2 - kortingsbonnen * 3)
+print("De feestlunch kost je bij de bakker " +  (str(crossaintjes * 17 + stokbroden * 2 - kortingsbonnen * 3 ) + " euro voor de crossaintjes en stokbroden als de kortingsbonnen nog geldig zijn")) 
--- de Speelhal-dag.py
+++ de Speelhal-dag.py
@@ -3,4 +3,5 @@ toegangsticket = 7.45 #euro
 VIPVRtijd = 45 #minuten
 VIPVRKoste = 0.37 #euro, het kost 0.37 per 5 minuten
 
-print(persoon * toegangsticket + VIPVRtijd / 5 * VIPVRKoste)
+
+print("Dit geweldige dagje-uit met 4 mensen in de Speelhal met 45 minuten VR kost je maar" + (str( persoon * toegangsticket + VIPVRtijd / 5 * VIPVRKoste) + " euro."))
